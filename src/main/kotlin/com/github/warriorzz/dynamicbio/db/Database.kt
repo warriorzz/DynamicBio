97c3b076bb3db8d1ff9535c23d132897816c3450
warrior_zz <warriormayer@gmail.com>
2021-04-27 11:49:01 +0200

    well idk

--- src/main/kotlin/com/github/warriorzz/dynamicbio/db/Database.kt
+++ src/main/kotlin/com/github/warriorzz/dynamicbio/db/Database.kt
@@ -32,7 +32,7 @@ object Database {
         database = client.getDatabase(Config.DATABASE_NAME)
         biographieCollection = database.getCollection()
         databaseScope.launch {
-            if (biographieCollection.find().toList().isEmpty()) {
+            if (biographieCollection.countDocuments() == 0L) {
                 biographieCollection.insertOne(Biographie("written by Leon", "github.com/warriorzz/dynamicbio", "Twitter"))
             }
         }
