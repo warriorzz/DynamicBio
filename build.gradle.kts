6b36d5780aa371ea7400377cafc33ea70e1305a0
warriorzz <bjar@gmx.de>
2021-05-31 22:54:56 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.5.0"
+    kotlin("jvm") version "1.5.10"
     kotlin("plugin.serialization") version "1.5.0"
     application
 }
@@ -13,17 +13,17 @@ repositories {
 
 dependencies {
     implementation("io.github.cdimascio", "dotenv-kotlin", "6.2.2")
-    implementation(platform("io.ktor:ktor-bom:1.5.4"))
+    implementation(platform("io.ktor:ktor-bom:1.6.0"))
     implementation("io.ktor", "ktor-client-okhttp")
     implementation("io.ktor", "ktor-client-serialization")
 
-    implementation("dev.inmo", "krontab", "0.5.2")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.5.0-RC")
+    implementation("dev.inmo", "krontab", "0.6.0")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.5.0")
     implementation("org.litote.kmongo", "kmongo-coroutine-serialization", "4.2.7")
-    implementation("io.github.microutils", "kotlin-logging", "2.0.6")
+    implementation("io.github.microutils", "kotlin-logging-jvm", "2.0.6")
     implementation("org.slf4j", "slf4j-api", "1.7.30")
 
-    val junitVersion = "5.7.1"
+    val junitVersion = "5.7.2"
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter", "junit-jupiter-api", junitVersion)
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", junitVersion)
