ec115a4cebf2bbc62f2688493ae637e78aedb15a
warrior <warriormayer@gmail.com>
2021-06-26 15:47:18 +0200

    Make values for discord module not necessary

--- src/main/kotlin/com/github/warriorzz/dynamicbio/config/Config.kt
+++ src/main/kotlin/com/github/warriorzz/dynamicbio/config/Config.kt
@@ -18,8 +18,8 @@ object Config {
 
     val ENABLE_DISCORD by getEnv(default = false) { it.toBoolean() }
     val DISCORD_API_URL by getEnv(default = "")
-    val DISCORD_GUILD_ID by environment
-    val DISCORD_USER_ID by environment
+    val DISCORD_GUILD_ID by getEnv(default = "")
+    val DISCORD_USER_ID by getEnv(default = "")
 }
 
 enum class Environment {
