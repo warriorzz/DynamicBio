0ad11ac7dd998ddd501544fa58c848168a23adfb
warrior <warriormayer@gmail.com>
2021-06-26 09:35:09 +0200

    Add modules to final biography

--- src/main/kotlin/com/github/warriorzz/dynamicbio/modules/impl/DiscordModule.kt
+++ src/main/kotlin/com/github/warriorzz/dynamicbio/modules/impl/DiscordModule.kt
@@ -5,7 +5,7 @@ import com.github.warriorzz.dynamicbio.config.Config
 import com.github.warriorzz.dynamicbio.modules.BiographyModule
 import io.ktor.client.request.*
 
-class DiscordModule : BiographyModule {
+object DiscordModule : BiographyModule {
     override suspend fun getModuleString(): String {
         return DynamicBio.httpClient.get(Config.DISCORD_API_URL) {
             parameter("userId", Config.DISCORD_USER_ID)
@@ -14,4 +14,5 @@ class DiscordModule : BiographyModule {
     }
 
     override fun isEnabled(): Boolean = Config.ENABLE_DISCORD
+    override fun getPriority(): Int = 0
 }
--- src/main/kotlin/com/github/warriorzz/dynamicbio/utils/BiographyProvider.kt
+++ src/main/kotlin/com/github/warriorzz/dynamicbio/utils/BiographyProvider.kt
@@ -2,6 +2,7 @@ package com.github.warriorzz.dynamicbio.utils
 
 import com.github.warriorzz.dynamicbio.db.Database
 import com.github.warriorzz.dynamicbio.model.Biography
+import com.github.warriorzz.dynamicbio.modules.impl.DiscordModule
 
 object BiographyProvider {
 
@@ -10,14 +11,22 @@ object BiographyProvider {
     )
     var lastBiography = list.random()
 
+    private val moduleList = mutableListOf(DiscordModule)
+    private const val SEPARATOR = " - "
+
     suspend operator fun invoke() {
         Database.biographyCollection.find().consumeEach {
             list.add(it)
         }
     }
 
-    fun getNextBio(): Biography {
+    suspend fun getNextBio(): Biography {
         lastBiography = list.filter { it != lastBiography }.random()
+        lastBiography.description = lastBiography.description + moduleList.filter {
+            it.isEnabled()
+        }.sorted().map {
+            it.getModuleString()
+        }.reduce { acc, s -> "$acc$SEPARATOR$s" }
         return lastBiography
     }
 }
